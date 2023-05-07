fc6c965fb8880305d2de041fb1a05d5fad528fb3
hyerim <hyyrim@gmail.com>
2023-05-07 21:46:20 +0900

    init calendar & add toggle function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,108 @@
-console.log("hello world!");
+class DatePicker {
+  monthData = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  #calendarDate = {
+    data: '',
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  selectedDate = {
+    data: '',
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  datePickerEl;
+  dateInputEl;
+  calendarEl;
+  calendarMonthEl;
+  monthContentEl;
+  nextBtnEl;
+  prevBtnEl;
+  calendarDatesEl;
+
+  constructor() {
+    this.initCalendarDate();
+    this.assignElement();
+    this.addEvent();
+  }
+
+  initCalendarDate() {
+    const data = new Date();
+    const date = data.getDate();
+    const month = data.getMonth();
+    const year = data.getFullYear();
+    this.#calendarDate = {
+      data,
+      date,
+      month,
+      year,
+    };
+  }
+
+  assignElement() {
+    this.datePickerEl = document.getElementById('date-picker');
+    this.dateInputEl = this.datePickerEl.querySelector('#date-input');
+    this.calendarEl = this.datePickerEl.querySelector('#calendar');
+    this.calendarMonthEl = this.calendarEl.querySelector('#month');
+    this.monthContentEl = this.calendarMonthEl.querySelector('#content');
+    this.nextBtnEl = this.calendarMonthEl.querySelector('#next');
+    this.prevBtnEl = this.calendarMonthEl.querySelector('#prev');
+    this.calendarDatesEl = this.calendarEl.querySelector('#dates');
+  }
+
+  addEvent() {
+    this.dateInputEl.addEventListener('click', this.toggleCalendar.bind(this));
+  }
+
+  toggleCalendar() {
+    this.calendarEl.classList.toggle('active');
+    this.updateMonth();
+    this.updateDates();
+  }
+  updateMonth() {
+    this.monthContentEl.textContent = `${this.#calendarDate.year} ${
+      this.monthData[this.#calendarDate.month]
+    }`;
+  }
+  updateDates() {
+    this.calendarDatesEl.innerHTML = '';
+    // 해당 년, 월의 full 날짜 구하기
+    const numberOfDates = new Date(
+      this.#calendarDate.year,
+      this.#calendarDate.month + 1,
+      0,
+    ).getDate();
+
+    const fragment = new DocumentFragment();
+    for (let i = 0; i < numberOfDates; i++) {
+      const dateEl = document.createElement('div');
+      dateEl.classList.add('date');
+      dateEl.textContent = i + 1;
+      dateEl.dataset.date = i + 1;
+      fragment.appendChild(dateEl);
+    }
+    fragment.firstChild.style.gridColumnStart =
+      new Date(this.#calendarDate.year, this.#calendarDate.month, 1).getDay() +
+      1;
+    this.calendarDatesEl.appendChild(fragment);
+  }
+}
+
+new DatePicker();
